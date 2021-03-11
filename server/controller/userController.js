38db3c00e853c63c85e948d9a10f687047363389
Yury_Verameichyk <Yury_Verameichyk@epam.com>
2021-03-11 20:19:51 +0300

    backend changes

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -32,27 +32,5 @@ class UserController {
             res.send({message: "Server error"})
         }
     }
-
-    async create(first_name, family_name, d_birth, d_death, cb) {
-        authordetail = {first_name:first_name , family_name: family_name }
-        if (d_birth != false) authordetail.date_of_birth = d_birth
-        if (d_death != false) authordetail.date_of_death = d_death
-
-        var author = new Author(authordetail);
-
-        author.save(function (err) {
-            if (err) {
-                cb(err, null)
-                return
-            }
-            console.log('New Author: ' + author);
-            authors.push(author)
-            cb(null, author)
-        }  );
-    }
 }
-
-
-
-
 module.exports = new UserController()
