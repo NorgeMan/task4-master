06ead0f703f392341d0f945853a9eb380fd425da
Yury_Verameichyk <Yury_Verameichyk@epam.com>
2021-03-17 18:11:04 +0300

    Table fixed

--- client/src/components/lists/Articles.jsx
+++ client/src/components/lists/Articles.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import {getBooks} from "../../actions/book";
 import {useDispatch, useSelector} from "react-redux";
 import MaterialTable, {MTableToolbar} from "material-table";
-import { forwardRef } from 'react';
-import {
-    AddBox, ArrowDownward,
-    Check, ChevronLeft,
-    ChevronRight, Clear,
-    DeleteOutline,
-    Edit,
-    FilterList,
-    FirstPage,
-    LastPage, Remove,
-    SaveAlt, Search, ViewColumn
-} from "@material-ui/icons";
+import {MTableIcons} from "../../utils/MTableWrapper";
 
 const Articles = () => {
     const dispatch = useDispatch()
@@ -23,26 +12,7 @@ const Articles = () => {
     }
     console.log(book_list);
 
-    const tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-    };
-
+    const tableIcons = MTableIcons();
     const columns = [
         {
             title: 'Title', field: 'title',
@@ -81,7 +51,7 @@ const Articles = () => {
     return (
         <div style={{height: '100%', width: '100%'}}>
             <MaterialTable title="Articles" data={rows} columns={columns}
-                           icons ={tableIcons}
+                           icons={tableIcons}
                            options={{search: true, paging: true, exportButton: true}}
                            onRowClick={(event, rowData) => rowClickedHandler(rowData)}/>
         </div>
