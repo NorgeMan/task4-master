605e2e8d290d2ac30d5fd62ded28e9bf369f4554
Yury_Verameichyk <Yury_Verameichyk@epam.com>
2021-03-17 15:37:18 +0300

    Table fixed

--- client/src/components/lists/Articles.jsx
+++ client/src/components/lists/Articles.jsx
@@ -1,29 +1,70 @@
 import React from 'react';
-import {useEffect} from "react/cjs/react.production.min";
 import {getBooks} from "../../actions/book";
 import {useDispatch, useSelector} from "react-redux";
-import MaterialTable from "material-table";
+import MaterialTable, {MTableToolbar} from "material-table";
+import { forwardRef } from 'react';
+import {
+    AddBox, ArrowDownward,
+    Check, ChevronLeft,
+    ChevronRight, Clear,
+    DeleteOutline,
+    Edit,
+    FilterList,
+    FirstPage,
+    LastPage, Remove,
+    SaveAlt, Search, ViewColumn
+} from "@material-ui/icons";
 
 const Articles = () => {
     const dispatch = useDispatch()
-    dispatch(getBooks());
-
     const book_list = useSelector(state => state.books);
-    console.log("2. found: ");
+    if (!book_list) {
+        dispatch(getBooks());
+    }
     console.log(book_list);
 
+    const tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };
+
     const columns = [
-        {title: 'Title', field: 'title'},
-        {title: 'Author', field: 'author.first_name'},
-        {title: 'ID', field: '_id'},
-        {title: 'Avatar',
-            field: 'author.first_name',
-            render: rowData => "<a href='/api/catalog/book/{rowData.id}'>{rowData.title}</a>"
+        {
+            title: 'Title', field: 'title',
+            render: rowData => {
+                let url = "/book/" + rowData._id;
+                return (<a href={url}>{rowData.title}</a>);
+            }
+        },
+        {
+            title: 'Author', field: 'author.first_name',
+            render: rowData => {
+                if (rowData.author) {
+                    let url = "/author/" + rowData.author._id;
+                    let authorName = rowData.author.first_name + " " + rowData.author.family_name;
+                    return (<a href={url}>{authorName}</a>);
+                }
+                return (<a href='/authors'>..</a>);
+            }
         }
     ];
     let rows = [];
     if (book_list) {
-        console.log("3. found:");
         console.log(book_list);
         rows = book_list['books'];
         if (rows) {
@@ -40,8 +81,15 @@ const Articles = () => {
     return (
         <div style={{height: '100%', width: '100%'}}>
             <MaterialTable title="Articles" data={rows} columns={columns}
-                           options={{search: true, paging: true, filtering: false, exportButton: true}}/>
+                           icons ={tableIcons}
+                           options={{search: true, paging: true, exportButton: true}}
+                           onRowClick={(event, rowData) => rowClickedHandler(rowData)}/>
         </div>
     );
 }
+
+function rowClickedHandler(rowData) {
+    console.log("/book/" + rowData._id);
+}
+
 export default Articles;
