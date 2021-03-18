1cd6a4e7bfe6902687f63ab685401051bb99e48b
NorgeMan <ivan.veremeichik@gmail.com>
2021-03-18 16:11:15 +0300

    Navbar search added fixed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './navbar.css'
-import Logo from '../../assets/img/navbar-logo.png'
+import Logo from '../../assets/img/Logo.png';
 import {NavLink} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import {logout} from "../../reducers/userReducer";
 import {useDispatch, useSelector} from "react-redux";
+import {MDBBtn, MDBCol, MDBFormInline} from "mdbreact";
+import SearchField from 'react-search-field';
 
 const Navbar = () => {
     const isAuth = useSelector(state => state.user.isAuth)
     const dispatch = useDispatch()
+
 
     return (
         <div className="navbar">
@@ -19,13 +22,17 @@ const Navbar = () => {
                 <div className="navbar__login"><NavLink to="/articles">Articles</NavLink></div>
                 <div className="navbar__login"><NavLink to="/authors">Authors</NavLink></div>
                 <div className="navbar__login"><NavLink to="/genres">Genres</NavLink></div>
-
+                <SearchField placeholder='Search' onSearchClick={onSearchClick}/>
                 {!isAuth && <div className="navbar__login"><NavLink to="/login">Login In</NavLink></div>}
                 {!isAuth && <div className="navbar__registration"><NavLink to="/registration">Sign Up</NavLink></div>}
-                {!isAuth && <div className="navbar__login" onClick={() => dispatchEvent(logout())}><NavLink to="/logout">Log out</NavLink></div>}
+                {!isAuth &&
+                <div className="navbar__login" onClick={() => dispatchEvent(logout())}><NavLink to="/logout">Log
+                    out</NavLink></div>}
             </div>
         </div>
     );
 };
+function onSearchClick(){
 
+}
 export default Navbar;
--- client/src/components/welcomepage/WelcomePage.jsx
+++ client/src/components/welcomepage/WelcomePage.jsx
@@ -14,52 +14,42 @@ const WelcomePage = () => {
     const dispatch = useDispatch()
 
     return (
-        <MDBCol md="12">
-            <MDBFormInline className="md-form mr-auto mb-4">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                <MDBBtn gradient="aqua" rounded size="sm" type="submit" className="mr-auto">
-                    Search
-                </MDBBtn>
-                <div>
-                    <Tabs>
-                        <div className="tabs">
-                            <Tab>
-                                <button className="tab-lists">Authors</button>
-                            </Tab>
-                            <Tab>
-                                <button className="tab-lists">Articles</button>
-                            </Tab>
-                            <Tab>
-                                <button className="tab-lists">Genres</button>
-                            </Tab>
-                        </div>
-                        <Panel>
-                            <p>
-                                Source: {' '}
-                                <a href="../lists/Authors.jsx">Authors</a>
-                            </p>
-                        </Panel>
-                        <Panel></Panel>
-                        <Panel></Panel>
-                    </Tabs>
+        <div>
+            <Tabs>
+                <div className="tabs">
+                    <Tab>
+                        <button className="tab-lists">Authors</button>
+                    </Tab>
+                    <Tab>
+                        <button className="tab-lists">Articles</button>
+                    </Tab>
+                    <Tab>
+                        <button className="tab-lists">Genres</button>
+                    </Tab>
                 </div>
-                <div className="container">
-                    <div className="item">1</div>
-                    <div className="item">2</div>
-                    <div className="item">3</div>
-                    <div className="item">4</div>
-                    <div className="item">5</div>
-                    <div className="item">6</div>
-                    <div className="item">7</div>
-                    <div className="item">8</div>
-                    <div className="item">9</div>
-                    <div className="item">10</div>
-                    <div className="item">11</div>
-                    <div className="item">12</div>
+                <Panel>
+                    <p>
+                        Source: {' '}
+                        <a href="../lists/Authors.jsx">Authors</a>
+                    </p>
+                </Panel>
+            </Tabs>
+            <div className="container">
+                <div className="item">1</div>
+                <div className="item">2</div>
+                <div className="item">3</div>
+                <div className="item">4</div>
+                <div className="item">5</div>
+                <div className="item">6</div>
+                <div className="item">7</div>
+                <div className="item">8</div>
+                <div className="item">9</div>
+                <div className="item">10</div>
+                <div className="item">11</div>
+                <div className="item">12</div>
+            </div>
+        </div>
 
-                </div>
-            </MDBFormInline>
-        </MDBCol>
     );
 };
 
