84cd4a360d9d7a1e84ac044fb572bbb5cc8f15fd
NorgeMan <ivan.veremeichik@gmail.com>
2021-03-22 20:52:33 +0300

    with network error

--- client/src/actions/axiosConfig.js
+++ client/src/actions/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://stark-ravine-53426.herokuapp.com'
+    baseURL: 'https://stark-ravine-53426.herokuapp.com'
     // baseURL: 'http://localhost:5000'
 });
 export default instance;
